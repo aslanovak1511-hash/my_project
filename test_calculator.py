d63f5674f1cf8dc4633491e4c1cd8d57d875b7d0
aslanovak1511-hash <aslanovak1511@gmail.com>
2026-05-14 17:32:20 +0300

    Initial commit: add calculator and tests

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        self.assertEqual(calc.add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
